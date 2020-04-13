8dc2e8618e6c80d7afe268486fa329112b247bf1
Lecky Lao <leckylao@gmail.com>
2020-04-14 01:22:07 +1000

    remove default App.css and logo.svg; adding unilogin/react to fix loading bug; adding rimble-ui and style-components;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import { ULWeb3Provider } from '@unilogin/web3';
+import Web3 from 'web3';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BaseStyles, Box, Button, Heading, Text } from "rimble-ui";
+
+const infuraToken = process.env.REACT_APP_INFURA_TOKEN || '95202223388e49f48b423ea50a70e336';
+
+const ulWeb3 = new ULWeb3Provider({
+    provider: new Web3.providers.WebsocketProvider(
+      `wss://kovan.infura.io/ws/v3/${infuraToken}`
+    ),
+    // relayerUrl: 'https://relayer-kovan.herokuapp.com',
+    ensDomains: ['gnosishack.test'],
+    applicationInfo: {
+      applicationName: 'Gnosis Hack',
+      logo: 'https://safe-transaction.gnosis.io/static/safe/favicon.png'
+      // type: 'laptop'
+    }
+})
+
+const web3 = new Web3(ulWeb3);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BaseStyles>
+      <Box>
+        <Heading>Your Heading</Heading>
+        <Text>Some of text of yours here</Text>
+        <Button> click me! </Button>
+      </Box>
+    </BaseStyles>
   );
 }
 
