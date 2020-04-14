6fd37864c10552b730a3a4e96b195903733cbeb1
Lecky Lao <leckylao@gmail.com>
2020-04-15 01:13:59 +1000

    adding v11.10.1 into .nvmrc; moving @testing-library into devDependencies; added web3, rimble/network-indicator and @unilogin/provider; Update App.js to login with UniLogin; Update index.js with BaseStyles;

--- src/App.js
+++ src/App.js
@@ -1,34 +1,68 @@
-import { ULWeb3Provider } from '@unilogin/web3';
-import Web3 from 'web3';
+import ULIFrameProvider from '@unilogin/provider';
+import Web3 from 'web3'
 import React from 'react';
-import { BaseStyles, Box, Button, Heading, Text } from "rimble-ui";
+import { Card, Form, Button, Text } from "rimble-ui";
+import NetworkIndicator from "@rimble/network-indicator";
 
-const infuraToken = process.env.REACT_APP_INFURA_TOKEN || '95202223388e49f48b423ea50a70e336';
+const ulProvider = ULIFrameProvider.create('rinkeby');
+console.log(ulProvider);
+const web3 = new Web3(ulProvider);
+console.log(web3);
 
-const ulWeb3 = new ULWeb3Provider({
-    provider: new Web3.providers.WebsocketProvider(
-      `wss://kovan.infura.io/ws/v3/${infuraToken}`
-    ),
-    // relayerUrl: 'https://relayer-kovan.herokuapp.com',
-    ensDomains: ['gnosishack.test'],
-    applicationInfo: {
-      applicationName: 'Gnosis Hack',
-      logo: 'https://safe-transaction.gnosis.io/static/safe/favicon.png'
-      // type: 'laptop'
-    }
-})
+let networkId = "Unknown";
+switch (ulProvider.config.network) {
+  case "mainnet":
+    networkId = 1;
+    break;
+  case "kovan":
+    networkId = 42;
+    break;
+  case "ropsten":
+    networkId = 3;
+    break;
+  case "rinkeby":
+    networkId = 4;
+    break;
+  case "goerli":
+    networkId = 5;
+    break;
+  default:
+    break;
+}
 
-const web3 = new Web3(ulWeb3);
+let asyncCall = async () => {
+  let result = await web3.eth.requestAccounts();
+  console.log(result);
+}
+
+asyncCall();
 
 function App() {
   return (
-    <BaseStyles>
-      <Box>
-        <Heading>Your Heading</Heading>
-        <Text>Some of text of yours here</Text>
-        <Button> click me! </Button>
-      </Box>
-    </BaseStyles>
+    <div>
+      <NetworkIndicator currentNetwork={networkId} requiredNetwork={4}>
+        {{
+          onNetworkMessage: "Connected to correct network",
+          noNetworkMessage: "Not connected to anything",
+          onWrongNetworkMessage: "Wrong network"
+        }}
+      </NetworkIndicator>
+      <Card bg={'background'}>
+        <Form display={'flex'} flexDirection={'column'}>
+          <Text fontSize={4} mb={4}>
+            Sign In
+          </Text>
+          <Form.Field label={'Username'}>
+            <Form.Input type={'text'}  width={1} />
+          </Form.Field>
+          <Form.Field label={'Password'}>
+            <Form.Input type={'password'}  width={1} />
+          </Form.Field>
+          <Form.Check label={'Remember Me'} mb={3} />
+          <Button>Sign In</Button>
+        </Form>
+      </Card>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BaseStyles } from "rimble-ui";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BaseStyles>
+      <App />
+    </BaseStyles>
   </React.StrictMode>,
   document.getElementById('root')
 );
